491964672b198c07dd1acf23269d6a0f00de2749
José Alejandro Aceituno Coello <jalejandroaceituno@unitec.edu>
2016-09-21 23:39:09 -0600

    arreglando un detalle anterior, ya me voy a dormir

--- app/scripts/controllers/calendarController.js
+++ app/scripts/controllers/calendarController.js
@@ -1,6 +1,6 @@
 angular.module('helpiApp.Controllers')
-  .controller('calendarController', ['ui.calendar','ui.bootstrap','reservacionService', '$scope', '$rootScope', '$sessionStorage', '$location', '$window',
-function (reservacionService, $scope, $rootScope, $sessionStorage, $location, $window, $compile, uiCalendarConfig) {
+  .controller('calendarController', ['reservacionService', '$scope', '$rootScope', '$sessionStorage', '$location', '$window',
+function (reservacionService, $scope, $rootScope, $sessionStorage, $location, $window) {
     $scope.Reserva = {};
     $scope.Reservas = [];
     var date = new Date();
@@ -21,17 +21,17 @@ function (reservacionService, $scope, $rootScope, $sessionStorage, $location, $w
 
     $scope.addReserva = function(Reserva){
 
-      var date_ini[] = Reserva.fecha_inicio.split("/");
-      var date_end[] = Reserva.fecha_fin.split("/");
-      var hour_ini[] = Reserva.hora_inicio.split(":");
-      var hour_end[] = Reserva.hora_fin.split(":");
+      var date_ini = Reserva.fecha_inicio.split("/");
+      var date_end = Reserva.fecha_fin.split("/");
+      var hour_ini = Reserva.hora_inicio.split(":");
+      var hour_end = Reserva.hora_fin.split(":");
 
       var initial = new Date(date_ini[0],date_ini[1],date_ini[2],hour_ini[0],hour_ini[1],0,0);
       var final = new Date(date_end[0],date_end[1],date_end[2],hour_end[0],hour_end[1],0,0);
 
       reservacionService.Add({
         idLab: Reserva.id,
-        Email: "ricardo.j.galdamez@gmail.com",
+        Email: "jalejandroaceituno@gmail.com",
         fecha_I:initial,
         fecha_F:final,
         Descripcion: Reserva.descripcion
